ad536cb54bdccbab4f0e807612595d1d6c2c11f0
combativeNinja <72140075+combativeNinja@users.noreply.github.com>
2021-11-18 17:07:24 +0530

    netflix landing page

---
+++ index.js
@@ -0,0 +1,16 @@
+let acc= document.getElementsByClassName('accordian');
+
+for(let i=0;i<acc.length;i++){
+
+    acc[i].addEventListener('click',function(){
+        this.classList.toggle("active");
+
+        let panel=this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight=null;
+        }
+        else{
+            panel.style.maxHeight=panel.scrollHeight+"px";
+        }
+    });
+}
